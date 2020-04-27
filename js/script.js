447d7a4f762cee8c30b334b2fb584c7beb30dc6a
denis <s.d-v@mail.ru>
2020-04-27 14:38:22 +0300

    Блок partner

--- js/script.js
+++ js/script.js
@@ -40,8 +40,8 @@ window.addEventListener('DOMContentLoaded', function () {
         console.log('prev');
     }
 
-    $('.new-items__arrow-prev').click(prevSlider);
-    $('.new-item__arrow-next').click(nextSlider);
+    $('.new-items__arrow-box .new-items__arrow-prev').click(prevSlider);
+    $('.new-items__arrow-box .new-items__arrow-next').click(nextSlider);
 
 
     const sliderTwo = $('.news .owl-carousel');
@@ -73,6 +73,44 @@ window.addEventListener('DOMContentLoaded', function () {
         // },
 
     }));
+    
+    $('.news__arrow-box .news__arrow-prev').click(function () {
+        sliderTwo.trigger('prev.owl.carousel');
+    });
+    $('.news__arrow-box .news__arrow-next').click(function () {
+        sliderTwo.trigger('next.owl.carousel');
+    });
+
+    const sliderThree = $('.partner .owl-carousel');
+    sliderThree.owlCarousel($.extend({}, {
+        nav: false,
+        dots: false,
+        margin: 20,
+        items: 7,
+        loop: true
+        // responsive: {
+        //     0: {
+        //         center: true,
+        //         margin: 32,
+        //         items: 2,
+        //         loop: true,
+        //     },
+        //     720: {
+        //         margin: 16,
+        //         items: 5
+        //     },
+        //     960: {
+        //         margin: 34,
+        //         items: 4
+        //     },
+        //     1440: {
+        //         margin: 10,
+        //         items: 5
+        //     }
+        // },
+
+    }));
+    
 
 
 });
